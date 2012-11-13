1a6bf7e4320de7baeb77aff39584965954f5125d
Chyld Medford <chyld.medford@gmail.com>
2012-11-13 11:00:26 -0500

    imdb2

---
+++ labs/imdb2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imdb2::Application.config.secret_token = 'e65eda2d89c34e6d2b5154077e298853bf25beb1ab565ea4cb6299bdb306a3c083c6c253a561d817fd827fd9d6b45ac0c4ea416f82a201a30ff28fa40dbb1322'
