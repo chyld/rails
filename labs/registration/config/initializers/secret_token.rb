6c71f168cbde201b52b5716c4e2eed168a3fc9bc
Chyld Medford <chyld.medford@gmail.com>
2012-11-14 08:53:00 -0500

    registration

---
+++ labs/registration/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Registration::Application.config.secret_token = '5b537170e527b4022424c076d242e47685c0a6bf15345bfdc47a4f41a1c379685774752c4f9d12b7874603abe1237386250b376a520630673d6148db6eee6f80'
---
+++ labs/stoq2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stoq2::Application.config.secret_token = '7889281c52932cb13efab00ee5b6298bff8a6844e96af5d75d0725b0516375aa958a455e70af221246c15c408eebe43764653903dd9058e020780553b8d57815'
