0846d9a4d0dd8967afe71212cd416ebe08731b16
Chyld Medford <chyld.medford@gmail.com>
2012-11-15 10:51:22 -0500

    debugging

--- labs/exercise_05/app/controllers/movies_controller.rb
+++ labs/exercise_05/app/controllers/movies_controller.rb
@@ -5,5 +5,6 @@ class MoviesController < ApplicationController
   def show_movie
     movie_names = params[:movies].split(', ')
     @movies = movie_names.map {|name| JSON(HTTParty.get("http://www.omdbapi.com/?t=#{name.gsub(' ', '+')}")) }
+    # binding.pry
   end
 end
