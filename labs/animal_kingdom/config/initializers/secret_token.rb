f5a91cc85ea5337b98bca5754d5a4d4db32cfe4f
Chyld Medford <chyld.medford@gmail.com>
2012-11-15 16:20:41 -0500

    animal kingdom

---
+++ labs/animal_kingdom/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimalKingdom::Application.config.secret_token = 'b5ffa1abe7f2d3076dcaeed4bb761fc72d58db83df4a5e799311895c72ad213d999b620d6c954d11a3a74c4f259725c8f55bb1dd0da8d173b736ad22dd8ddae9'
