3255a37b869e644fd2fc5ff73410dd0566ff07c5
Chyld Medford <chyld.medford@gmail.com>
2012-11-19 13:27:11 -0500

    dogs

---
+++ labs/muzikdemo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muzikdemo::Application.config.secret_token = 'f3879b07429de10e520bfe35849f71040ee6d6399c07518aa727ae4b60aadb3abcdde137a6839c3fc1899bc5211355c4490d73f6f51f3f54310bbe09944cf9f7'
