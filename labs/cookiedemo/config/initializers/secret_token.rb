e8af05d59c3baa6d058d03bbf1aec23638637ccf
Chyld Medford <chyld.medford@gmail.com>
2012-11-20 14:18:14 -0500

    cookiedemo

---
+++ labs/cookiedemo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookiedemo::Application.config.secret_token = '18f5f5ab13800dfa2b58c5d41d20c1a647b4a18f2764228c7342bc62d1c24fec825d04a9f477e431fd6923aa3fd10368f21ee42442f3d4a87a24592aa861a5d7'
