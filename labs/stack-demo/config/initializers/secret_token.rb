488029be024824fd807d2295f1cf2f4482a7605c
Chyld Medford <chyld.medford@gmail.com>
2012-12-03 11:25:46 -0500

    stacked

---
+++ labs/stack-demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackDemo::Application.config.secret_token = 'cddaa3dbadbfee4a9aeb4eaa8555ec0dd83156681e76712606cdff7bc23c35a723f4f0c16d6b0fad9320c7f6af65469e448a36c65feaeff4363c4188c8f262b8'
