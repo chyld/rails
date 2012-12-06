605fb0697c39dcc7298c9ef5d988b680e9f2a310
Chyld Medford <chyld.medford@gmail.com>
2012-12-06 16:10:28 -0500

    photo

---
+++ labs/js-graphs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsGraphs::Application.config.secret_token = 'fe21ad9ccaeacc9d7b72355f4d2fc7de3c6cb74f76a81a8bb88c26de36cb3c4fd4d1bcde609f0eb83eaf240b94769d728778d93467a73624ccc5a731eca00b6f'
---
+++ labs/photobooth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photobooth::Application.config.secret_token = '5eea93ddfa622e1774ad852caa41e711b1e18b005ea50067eedd71f6fcde566340595b160582bcda4d6e662dfe6f01112792cf5b03c0c760da06d112007fb046'
