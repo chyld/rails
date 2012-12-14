6c5cf80f9f6fddb7e8f6ed331ec0e47fece6033d
Chyld Medford <chyld.medford@gmail.com>
2012-12-14 01:57:27 -0500

    debugging

---
+++ labs/debugging_vs_ninjas/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DebuggingVsNinjas::Application.config.secret_token = 'd64796901f3ea685ef0c6132777537e199d8aa07b04a641876baaa94c4ece7398b5f6b1b7a770540ac430b068f797444963c3ae7a6163bf0f650ac2438f64405'
