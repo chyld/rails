00f316e736b297ea4947914de0d84ca3d682ea39
Chyld Medford <chyld.medford@gmail.com>
2012-12-19 10:06:58 -0500

    haml

---
+++ labs/haml_vs_vampires/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamlVsVampires::Application.config.secret_token = '5d340a6bfe0132beba02aadd7d1774007549b47b2086b8313d8061f6e1c5e560b925e564d62083e715b79a1a321d2f2f6227f79d33719a33e9e01451ef917d75'
